7be7b2b5aa2f54fd7dc511e3d3c9b6c6f05e8fa3
zyadhany <zeyad.hany2003@gmail.com>
2023-10-17 17:33:14 +0300

    added segment tree function

---
+++ Topics/reference/Trees/Segment_Tree.cpp
@@ -0,0 +1,92 @@
+/**
+ * usage:-
+ * creat tree element.
+ * SegmentTree sg;
+ * 
+ * Functions you can use:
+ * @set: set index to value.
+ * @geteange: get summ of given range.
+ * @build: build tree with given vector.
+ * 
+ * make sure to look at item typedef.
+ * you can change merge function to change it's oppration.
+*/
+
+typedef long long item;
+
+class SegmentTree
+{
+public:
+
+    void set(int index, int value) {
+        set(0, 0, size - 1, index, value);
+    }
+
+    item getrange(int l, int r) {
+        return (getrange(0, 0, size - 1, l, r));
+    }
+
+    void build(vector<item>& X) {
+        size = 1;
+        while (size < X.size())
+            size *= 2;
+        tree.assign(size * 2, 0);
+
+        build(X, 0, 0, size - 1);
+    }
+
+private:
+    int size;
+    vector<item> tree;
+
+    item merge(item a, item b) {
+        return (a + b);
+    }
+
+    void set(int m, int lx, int rx, int pos, int val) {
+        if (pos < lx || rx < pos) return;
+        if (lx == rx && lx == pos)
+        {
+            tree[m] = val;
+            return;
+        }
+        
+        int mid = (lx + rx) / 2;
+        item s1, s2;
+
+        set(m * 2 + 1, lx, mid, pos, val);
+        set(m * 2 + 2, mid + 1, rx, pos, val);
+        s1 = tree[m * 2 + 1], s2 = tree[m * 2 + 2];
+
+        tree[m] = merge(s1, s2);
+    }
+
+    item getrange(int m, int lx, int rx, int l, int r) {
+        if (rx < l || r < lx) return (0);
+        if (l <= lx && rx <= r) return (tree[m]);
+
+        int mid = (lx + rx) / 2;
+        item s1, s2;
+
+        s1 = getrange(m * 2 + 1, lx, mid, l, r);
+        s2 = getrange(m * 2 + 2, mid + 1, rx, l, r);
+
+        return merge(s1, s2);
+    }
+
+    void build(vector<item>& X, int m, int lx, int rx) {
+        if (lx == rx) {
+            if (lx < X.size()) tree[m] = X[lx];
+            return;
+        }
+
+        int mid = (lx + rx) / 2;
+        item s1, s2;
+
+        build(X, m * 2 + 1, lx, mid);
+        build(X, m * 2 + 2, mid + 1, rx);
+        s1 = tree[m * 2 + 1], s2 = tree[m * 2 + 2];
+
+        tree[m] = merge(s1, s2);
+    }
+};
