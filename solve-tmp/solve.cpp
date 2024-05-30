9e4143b8634f4fabb3d6049a61eba0c5f9335767
zyadhany <zeyad.hany2003@gmail.com>
2024-05-30 14:18:55 +0300

    #2

--- solve-tmp/solve.cpp
+++ solve-tmp/solve.cpp
@@ -2,7 +2,7 @@
 #include <bits/stdc++.h>
 #include <unordered_map>
 
-#define ll int
+#define ll long long
 #define ld long double
 #define pl pair<ll, ll>
 #define vi vector<ll>
@@ -27,16 +27,41 @@ using namespace std;
  
 
 void solve(int tc) {
-    ll n;
+    ll n, mn;
+    string s;
 
-    cin >> n;
+    cin >> s;
 
-    cout << n - 1 << ' ';
-    for (int i = 1; i < n; i++)
+    n = s.size();
+
+    vii X(n + 1, vi(n + 1, INT32_MAX));
+
+    X[0][n - 1] = 0;
+    for (int i = 0; i < n; i++)
     {
-        cout << "0 ";
+        for (int j = n - 1; j > i; j--)
+        {
+            if (s[i] == s[j]) X[i + 1][j - 1] = min(X[i + 1][j - 1], X[i][j]);
+            else
+            {
+                X[i + 1][j] = min(X[i + 1][j], X[i][j] + 1);
+                X[i][j - 1] = min(X[i][j - 1], X[i][j] + 1);
+            }
+            
+        }
     }
-    cout << '\n';
+    
+    mn = INT32_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i >= j) mn = min(mn, X[i][j]);
+        }
+        
+    }
+    
+    cout << mn << '\n';
 }
 
 
@@ -47,7 +72,7 @@ int main()
     //freopen("mex.in", "r", stdin);
     //freopen("output.txt", "w", stdout);
 
-    // cin >> size;
+    cin >> size;
     for (int i = 1; i <= size; i++)
         solve(i);
 }
