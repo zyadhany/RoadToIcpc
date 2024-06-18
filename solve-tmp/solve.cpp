d3cac767c05785e2c9e2b0e085ffe97302cf1922
zyadhany <zeyad.hany2003@gmail.com>
2024-06-18 13:52:25 +0300

    added range query

--- solve-tmp/solve.cpp
+++ solve-tmp/solve.cpp
@@ -21,157 +21,9 @@ using namespace std;
  
 const int MODE = 1e9 + 7;
 
-
-/**
- * usage:-
- * creat tree element.
- * SegmentTree sg;
- * 
- * Functions you can use:
- * @set: set index or range to value.
- * @geteange: get value of given range.
- * @build: build tree with given vector or size.
- * 
- * make sure to look at item typedef.
- * you can change merge function to change it's oppration.
- * it you want to make change to segment work in checkLazy().
-*/
-
-typedef long long item;
-/*
-struct item
+struct Query
 {
-    int val;
-
-    item(){
-        val = 0;
-    }
-};
-*/
-
-class SegmentTree
-{
-public:
-
-    void set(int index, int value) {
-        set(0, 0, size - 1, index, value);
-    }
-    void set(int l, int r, int value) {
-        set(0, 0, size - 1, l, r, value);
-    }
-
-    item getrange(int l, int r) {
-        return (getrange(0, 0, size - 1, l, r));
-    }
-
-    void build(int n) {
-        size = 1;
-        while (size < n)
-            size *= 2;
-        tree.assign(size * 2, item());
-        lazy.assign(size * 2, 0);
-    }
-
-    void build(vector<item>& X) {
-        size = 1;
-        while (size < X.size())
-            size *= 2;
-        tree.assign(size * 2, item());
-        lazy.assign(size * 2, 0);
-
-        build(X, 0, 0, size - 1);
-    }
-
-private:
-    int size;
-    vector<item> tree;
-    vector<long long> lazy;
-
-    item merge(item a, item b) {
-        item res = (a ^ b);
-        return (res);
-    }
-
-    void checkLazy(int m, int lx, int rx) {
-        if (!lazy[m]) return;
-        tree[m] += lazy[m];
-        
-        if (lx != rx) {
-            lazy[2 * m + 1] += lazy[m];
-            lazy[2 * m + 2] += lazy[m];
-        }
-
-        lazy[m] = 0;
-    }
-
-    void set(int m, int lx, int rx, int pos, int val) {
-        checkLazy(m, lx, rx);
-        if (pos < lx || rx < pos) return;
-        if (lx == rx && lx == pos)
-        {
-            tree[m] = val;
-            return;
-        }
-
-        int mid = (lx + rx) / 2;
-        item s1, s2;
-
-        set(m * 2 + 1, lx, mid, pos, val);
-        set(m * 2 + 2, mid + 1, rx, pos, val);
-        s1 = tree[m * 2 + 1], s2 = tree[m * 2 + 2];
-
-        tree[m] = merge(s1, s2);
-    }
-
-    void set(int m, int lx, int rx, int l, int r, int val) {
-        checkLazy(m, lx, rx);
-        if (rx < l || r < lx) return;
-        if (l <= lx && rx <= r)
-        {
-            lazy[m] = val;
-            checkLazy(m, lx, rx);
-            return;
-        }
-
-        int mid = (lx + rx) / 2;
-        item s1, s2;
-
-        set(m * 2 + 1, lx, mid, l, r, val);
-        set(m * 2 + 2, mid + 1, rx, l, r, val);
-        s1 = tree[m * 2 + 1], s2 = tree[m * 2 + 2];
-
-        tree[m] = merge(s1, s2);
-    }
-
-    item getrange(int m, int lx, int rx, int l, int r) {
-        checkLazy(m, lx, rx);
-        if (rx < l || r < lx) return (0);
-        if (l <= lx && rx <= r) return (tree[m]);
-
-        int mid = (lx + rx) / 2;
-        item s1, s2;
-
-        s1 = getrange(m * 2 + 1, lx, mid, l, r);
-        s2 = getrange(m * 2 + 2, mid + 1, rx, l, r);
-
-        return merge(s1, s2);
-    }
-
-    void build(vector<item>& X, int m, int lx, int rx) {
-        if (lx == rx) {
-            if (lx < X.size()) tree[m] = X[lx];
-            return;
-        }
-
-        int mid = (lx + rx) / 2;
-        item s1, s2;
-
-        build(X, m * 2 + 1, lx, mid);
-        build(X, m * 2 + 2, mid + 1, rx);
-        s1 = tree[m * 2 + 1], s2 = tree[m * 2 + 2];
-
-        tree[m] = merge(s1, s2);
-    }
+    int L, R, ind;
 };
 
 void solve(ll tc) {
@@ -179,19 +31,60 @@ void solve(ll tc) {
 
     cin >> n >> q;
 
-    vi X(n + 1);
-    SegmentTree sg;
+    vi X(n + 10);
+    vi Z(n + 10);
+    vector<vp> Q(n + 10);
+    vi res(q);
 
     for (int i = 1; i <= n; i++)
+    {
         cin >> X[i];
-    sg.build(X);
-
-    while (q--)
+        Z[i] = Z[i - 1] + X[i];
+    }
+    
+    for (int i = 0; i < q; i++)
     {
         ll l, r;
         cin >> l >> r;
-        cout << sg.getrange(l, r) << '\n';
+        Q[l].push_back({r, i});
     }
+    
+    vp st(1, {INT32_MAX, n + 1});
+    vi pref(1, 0);
+    for (int i = n; i >= 1; i--)
+    {
+        while (!st.empty() && st.back().first <= X[i])
+        {
+            st.pop_back();
+            pref.pop_back();
+        }
+        ll lst = st.back().second;
+        ll cont = X[i] * (lst - i) - (Z[lst - 1] - Z[i - 1]);
+
+        st.push_back({X[i], i});    
+        pref.push_back(pref.back() + cont);
+        
+        for (auto m : Q[i]) {
+            int ind = m.second;
+            ll summ = 0;
+            ll l, r, at;
+            l = 0, r = st.size() - 1;
+            while (l < r)
+            {
+                at = (l + r) / 2;
+                if (st[at].second > m.first) l = at + 1;
+                else r = at;
+            }
+            r = st[l].second;
+            summ = pref.back() - pref[l];
+            summ += (m.first - r + 1) * X[r] - (Z[m.first] - Z[r - 1]);
+            
+            res[ind] = summ;
+        }
+    }
+    
+    for (int i = 0; i < q; i++)
+        cout << res[i] << '\n';
 }
  
 int main()
