1414cbb44e5075bb3437c93424c26dced904684a
zyadhany <zeyad.hany2003@gmail.com>
2024-08-01 20:26:04 +0300

    .

--- solve-tmp/solve.cpp
+++ solve-tmp/solve.cpp
@@ -22,41 +22,86 @@ const int MODE = 1e9 + 7;
 
 using namespace std;
 
-void solve(int tc) {
-    string s;
-    cin >> s;
 
-    ll res = 0;
+class SegmentTree
+{
+public:
 
-
-    unordered_map<int, vi> M;
-    M.reserve(s.size() + 1);
-    M[0].push_back(0);
-
-    ll pref = 0;
-    for (int i = 0; i < s.size(); i++)
-    {
-        if (s[i] == '0') pref--;
-        else pref++;
-        M[pref].push_back(i + 1);
+    pl getrange(int l, int r, ll k, ll cnt) {
+        return (getrange(0, 0, size - 1, l, r, k, cnt));
     }
-    
-    for (auto &m : M) {
-        vi X = m.second;
-        ll n = X.size();
-        vi suff(n);
-        suff[n - 1] = (s.size() + 1 - X.back());
-        for (int i = n - 2; i >= 0; i--)
-            suff[i] = (suff[i + 1] + (s.size() + 1 - X[i])) % MODE;
-        
-        for (int i = 0; i < n - 1; i++)
-        {
-            ll re = ((X[i] + 1) * (suff[i + 1])) % MODE;
-            res = (res + re) % MODE;
+
+    void build(vi& X) {
+        size = 1;
+        while (size < X.size())
+            size *= 2;
+        tree.assign(size * 2, item());
+        build(X, 0, 0, size - 1);
+    }
+
+private:
+    int size;
+    vector<item> tree;
+    vector<long long> lazy;
+
+    pl getitemval(item& a, ll k) {
+        ll l;
+        l = lower_bound(all(a.arr), k) - a.arr.begin();
+        return (a.pref[l]);
+    }
+
+    item merge(item& a, item& b) {
+        item res = a;
+        for (int i = 0; i < b.arr.size(); i++)
+            res.arr.push_back(b.arr[i]);
+        sortx(res.arr);
+
+        res.pref.assign(res.arr.size() + 1, 0);
+
+        for (int i = 1; i <= res.arr.size(); i++)
+            res.pref[i] = res.pref[i - 1] + res.arr[i - 1];
+
+        return (res);
+    }
+
+    pl getrange(int m, int lx, int rx, int l, int r, ll k, ll cnt) {
+        if (rx < l || r < lx) return {0, -1};
+        if (l <= lx && rx <= r) {
+
+            return (getitemval(tree[m], k));
         }
+
+        int mid = (lx + rx) / 2;
+        pl s1, s2, res;
+
+        s1 = getrange(m * 2 + 1, lx, mid, l, r, k, cnt);
+
+        if (s1.second == cnt) return (s1);
+        return getrange(m * 2 + 2, mid + 1, rx, l, r, k, cnt - s1.second);
     }
 
-    cout << res << '\n';
+    void build(vi& X, int m, int lx, int rx) {
+        if (lx == rx) {
+            if (lx < X.size())
+                tree[m].arr.push_back(X[lx]), tree[m].pref.push_back(X[lx]);
+            return;
+        }
+
+        int mid = (lx + rx) / 2;
+        item s1, s2;
+
+        build(X, m * 2 + 1, lx, mid);
+        build(X, m * 2 + 2, mid + 1, rx);
+        s1 = tree[m * 2 + 1], s2 = tree[m * 2 + 2];
+
+        tree[m] = merge(s1, s2);
+    }
+};
+
+void solve(int tc) {
+    ll n;
+
+    cin >> n;
 }
 
 int main()
