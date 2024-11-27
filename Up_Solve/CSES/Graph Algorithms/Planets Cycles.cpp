12ef9d67116ee74d676665b64eb15dd318149956
zyadhany <zeyad.hany2003@gmail.com>
2024-11-27 15:10:16 +0200

    function graph

--- Up_Solve/CSES/Graph Algorithms/Planets Cycles.cpp
+++ Up_Solve/CSES/Graph Algorithms/Planets Cycles.cpp
@@ -25,26 +25,44 @@ const int MODE = 1e9;
 
 using namespace std;
 
+ll iscyc;
+
+ll req(vi &X, vi &vis, ll n, ll h) {
+    ll &res = vis[n];
+    if (res < 0) {
+        res += h;
+        iscyc = n;
+    }
+    if (res) return res;
+    res = -h;
+
+    res = req(X, vis, X[n], h + 1) + (iscyc == 0);
+    if (iscyc == n) iscyc = 0;
+    
+    return res;
+}
+
 void solve(int tc) {
     ll n;
 
     cin >> n;
 
-    vp X(n);
+    vi X(n + 1);
+    vi res(n + 1);
 
-    for (int i = 0; i < n; i++)
-        cin >> X[i].first >> X[i].second;
-    sortx(X);
-
-    ll cnt = 0;
-    ll ls = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (X[i].first >= ls) ls = X[i].second, cnt++;
-        else ls = min(ls, X[i].second);
-    }
+    for (int i = 1; i <= n; i++)
+        cin >> X[i];
     
-    cout << cnt << '\n';    
+    for (int i = 1; i <= n; i++)
+    {
+        if (res[i]) continue;
+        iscyc = 0;
+        req(X, res, i, 1);
+    }
+
+    for (int i = 1; i <= n; i++)
+        cout << res[i] << ' ';
+    cout << '\n';
 }
 
 int main()
