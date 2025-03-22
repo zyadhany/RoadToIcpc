9d6f644af38946029cd5a5c2c71c8dff495f9893
zyadhany <zeyad.hany2003@gmail.com>
2025-03-23 01:47:59 +0200

    unordered map

--- solve-tmp/solve.cpp
+++ solve-tmp/solve.cpp
@@ -27,79 +27,31 @@ const int MODE = 1e9 + 7;
 
 using namespace std;
 
-vi dx = {0, 0, 1, -1};
-vi dy = {1, -1, 0, 0};
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
 
-ll req(vii &adj, vi &match, vi &vis, ll n) {
-    if (vis[n]) return 0;
-    vis[n] = 1;
-    
-    for (auto v : adj[n]) {
-        if (match[v] == -1 || (!vis[match[v]] && req(adj, match, vis, match[v]))) {
-            match[n] = v;
-            match[v] = n;
-            return 1;
-        }
-    }
-    return 0;
-}
-
-ll maxMatching(vii &adj, ll sz) {
-    vi match(sz, -1);
-    ll result = 0;
-    
-    for (ll i = 0; i < sz; i++) {
-        vi vis(sz, 0);
-        if (match[i] == -1) result += req(adj, match, vis, i);
-    }
-    
-    return result;
-}
+// macros to define set 
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
 
 void solve(int tc) {
-    ll n, m;
+    ll n;
 
-    cin >> n >> m;
+    cin >> n;
 
-    ll sz = n* m;
-    vi X(sz);
+    ordered_set ord;
 
-    for (int i = 0; i < n; i++)
+    while (n--)
     {
-        for (int j = 0; j < m; j++)
-        {
-            char a; cin >> a;
-            if (a == 'G') X[i*m+j] = 1;
-            else if (a == 'W') X[i*m+j] = 2;
-            else X[i*m+j] = 0;
-        }
-    }
+        char c; ll k; cin >> c >> k;
 
-    vii adj(sz);
-    vi deg(sz);
-    vi tk(sz);
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if (X[i*m+j] != 2) continue;
-            for (int d = 0; d < 4; d++)
-            {
-                ll l = i + dx[d];
-                ll r = j + dy[d];
-                if (l < 0 || r < 0 || l >= n || r >= m) continue;
-                if (X[l*m+r] == 1) {
-                    deg[i*m+j]++;
-                    deg[l*m+r]++;
-                    adj[i*m+j].push_back(l*m+r);
-                    adj[l*m+r].push_back(i*m+j);
-                }
-            }
-        }
+        if (c == "I") ord.insert(k);
+        else if (c == "D") {
+            if (ord.count(k)) ord.erase(k);
+        } else if (c == "K") cout << *ord.find_by_order(k-1) << '\n';
+        else cout << ord.order_of_key(k) + 1 << '\n';
     }
     
-    cout << maxMatching(adj, sz) << '\n';
 }
  
 int main()
