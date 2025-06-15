791ad277a5ca62a1cdafd54a0a989301f3417e80
zyadhany <zeyad.hany2003@gmail.com>
2025-06-15 15:36:07 +0300

    dp optmization

--- solve-tmp/solve.cpp
+++ solve-tmp/solve.cpp
@@ -18,32 +18,60 @@
 #define all(X) X.begin(),X.end()
 #define allr(X) X.rbegin(),X.rend()
 #define ln '\n'
-#define YES {cout << "YES\n"; return;}
-#define NO {cout << "NO\n"; return;}
+#define YES {cout << "Yes\n"; return;}
+#define NO {cout << "No\n"; return;}
 #define MUN {cout << "-1\n"; return;}
 using namespace std;
  
 const int MODE = 1e9+7;
 
-void solve(int tc) {
-    ll n, st, en;
-    cin >> n >> st >> en;
+void add (ll &a, ll b) {
+    a = (a + b) % MODE;
+}
 
+void solve(int tc) {
+    ll st, en;
+    string s; 
+
+    cin >> s;
+    cin >> st >> en;
+
+    ll n = s.size();
     vii dp(n+1, vi(n+1));
-    dp[1][1] = 1;
+    dp[1][1] = (s[0] != 'L' || en == 1);
 
     for (int i = 1; i < n; i++)
     {
         for (int j = 1; j <= i; j++)
         {
-            if (i == st-1 || i == en-1) {
-                dp[i+1][j+1] = (dp[i+1][j+1]+ dp[i][j]) % MODE;
-                dp[i+1][j] = (dp[i+1][j] + dp[i][j]) % MODE;
-            } else {
-                dp[i+1][j+1] = (dp[i+1][j+1] + (j+1 - (i >= st) - (i >= en)) * dp[i][j]) % MODE;
-                dp[i+1][j-1] = (dp[i+1][j-1] + (j-1) * dp[i][j]) % MODE;
+            if (i == st-1) {
+                if (s[i-1] == 'L') {
+                    add(dp[i+1][j], dp[i][j]);
+                } else if (s[i-1] == 'R') {
+                    add(dp[i+1][j+1], dp[i][j]);
+                } else {
+                    add(dp[i+1][j], dp[i][j]);
+                    add(dp[i+1][j+1], dp[i][j]);
+                }    
+                continue;
             }
-        }
+            if (i == en-1) {
+                add(dp[i+1][j], dp[i][j]);
+                add(dp[i+1][j+1], dp[i][j]);
+                continue;
+            }
+
+            if (s[i-1] == 'L') {
+                add(dp[i+1][j], (j - (i>=st)) * dp[i][j]);
+            } else if (s[i-1] == 'R') {
+                add(dp[i+1][j], (j - (i>=en)) * dp[i][j]);
+                add(dp[i+1][j+1], (j+1-(i>=st)-(i>=en)) * dp[i][j]);
+            } else {
+                add(dp[i+1][j], (2*j - (i>=en) - (i>=st)) * dp[i][j]);
+                add(dp[i+1][j-1], (j-1) * dp[i][j]);
+                add(dp[i+1][j+1], (j+1-(i>=st)-(i>=en)) * dp[i][j]);
+            }
+        }        
     }
     
     cout << dp[n][1] << '\n';
--- tmp/d.cpp
+++ tmp/d.cpp
@@ -18,82 +18,161 @@
 #define all(X) X.begin(),X.end()
 #define allr(X) X.rbegin(),X.rend()
 #define ln '\n'
-#define YES {cout << "YES\n"; return;}
-#define NO {cout << "NO\n"; return;}
+#define YES {cout << "Yes\n"; return;}
+#define NO {cout << "No\n"; return;}
 #define MUN {cout << "-1\n"; return;}
 using namespace std;
+ 
+const int MODE = 1e9+7;
 
-struct query{
-    int u, v, c, r;
-    query(ll u, ll v, ll c, ll r) : u(u), v(v), c(c), r(r){}
-    bool operator<(const query& q) {
-        return c < q.c;
+class Graph {
+public:
+    int size, SPSIZE;
+    vi Indeg, lvl;
+    vi P;
+    vp Trav;
+    vector<vp> table;
+
+    void dfs( const vii &adj, int n, int p, ll deep) {
+        lvl[n] = deep;
+        P[n] = p;
+        Indeg[n] = Trav.size();
+        Trav.push_back({deep, n});
+        for (auto neg : adj[n]) {
+            if (neg == p) continue;
+            dfs(adj, neg, n, deep + 1);
+            Trav.push_back({deep, n});
+        }
+    }
+
+    void BuildLCA(const vii &adj) {
+        dfs(adj, 1, 0, 1);
+        ll n = Trav.size();
+        SPSIZE = ceil(log2(n));
+        table.resize(n, vp(SPSIZE + 1));
+        for (int i = 0; i < n; i++)
+            table[i][0] = Trav[i];
+        
+        for (int j = 1; j <= SPSIZE; j++)
+            for (int i = 0; i <= n - (1 << j); i++)
+                table[i][j] = min(table[i][j - 1], table[i + (1 << (j - 1))][j - 1]);
+    }
+
+    ll LCA(ll a, ll b) {
+        ll l = Indeg[a], r = Indeg[b];
+        if (l > r) swap(l, r);
+        int j = (int)log2(r - l + 1); 
+        return min(table[l][j], table[r - (1 << j) + 1][j]).second;
+    }
+
+    ll dist(ll a, ll b) {
+        return lvl[a] + lvl[b] - 2 * lvl[LCA(a, b)];
+    }
+
+    Graph(ll n, const vii &adj) {
+        size = n;
+        P.resize(n + 1, 0);
+        lvl.resize(n + 1, 0);
+        Indeg.assign(n + 1, 0);
+        BuildLCA(adj);
     }
 };
 
 void solve(int tc) {
-    ll n, m;
+    ll n, q, st, en;
 
-    cin >> n >> m;
-    vector<query> Q;
-    
-    vi X(n);
-    for (int i = 0; i < n; i++)
+    cin >> n >> q >> st >> en;
+
+    vii adj(n + 1);
+    for (int i = 0; i < n-1; i++)
     {
-        cin >> X[i];
+        ll u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
-    
-    for (int i = 0; i < m; i++)
-    {
-        ll u, v, c, r; cin >> u >> v >> c >> r;
-        u--, v--;
-        Q.push_back(query(u, v, c, r));
-    }
-    sortx(Q);
+       
+    Graph g(n, adj);
 
-    vector<vector<vector<query>>> Y(n, vector<vector<query>>(n));
-
-    for (auto p : Q) {
-        while (!Y[p.u][p.v].empty() && Y[p.u][p.v].back().r >= p.r) Y[p.u][p.v].pop_back();
-        Y[p.u][p.v].push_back(p);
-    }
-
-    ll l = 1, r = 1e18;
-    while (l < r)
-    {
-        ll mid = (l+r)>>1;
-        bool isok = 1;
-
-        for (int u = 0; u < n; u++)
-        {
-            for (int v = 0; v < n; v++)
-            {
-                cout << mid << "|\n"; 
-                for (auto &p : Y[u][v]) {
-                    ll tank = mid, ref = 0;
-
-                    for (int h = u+1; h <= v; h++)
-                    {
-                        tank -= (X[h]-X[h-1])*p.c;
-                        if (tank < 0) {
-                            tank = mid - (X[h]-X[h-1])*p.c;
-                            ref++;
-                            if (ref > p.r || tank < 0) {
-                                isok = 0;
-                                goto out;
-                            }
-                        }
-                    }
-                }
-            }
+    auto getpath = [&](ll a, ll b) -> vi {
+        vi Z;
+        ll lc = g.LCA(a, b);
+        while (a != lc) {
+            Z.push_back(a);
+            a = g.P[a];
         }
-        out:;
+        Z.push_back(lc);
+        vi tmp;
+        while (b != lc) {
+            tmp.push_back(b);
+            b = g.P[b];
+        }
+        reverse(all(tmp));
+        for (auto x : tmp) Z.push_back(x);
+        return Z;
+    };
 
-        if (isok) r = mid;
-        else l = mid+1;
+    vii forb(n+1, vi(1, 0));
+
+    while (q--) {
+        ll a, b;
+        cin >> a >> b;
+        vi P = getpath(a, b);
+        for (int i = 0; i < P.size(); i++)
+        {
+             forb[P[i]].push_back(i+1);
+        }
     }
     
-    cout << l << '\n';
+    for (int i = 0; i <= n; i++)
+    {
+        sortx(forb[i]);
+    }
+    auto Z = getpath(st, en);
+    
+    ll m = Z.size();
+    vector<vp> AV(m);
+    AV[0] = {{1, 3*n}};
+    if (forb[st].size() > 1) {
+        AV[0][0].second = forb[st][1]-1;
+    }
+
+    cout << AV[0][0].first << ' ' << AV[0][0].second << '\n';
+    for (int i = 1; i < m; i++)
+    {
+        if (AV[i-1].empty()) continue;
+        for (int j = 1; j < forb[Z[i]].size(); j++)
+        {
+            ll r = forb[Z[i]][j]-1;
+            ll l = forb[Z[i]][j-1] + 1;
+            if (l > r) continue;
+            ll ind = lower_bound(all(AV[i-1]), pl(l, 0)) - AV[i-1].begin();
+
+            if (ind > 0 && AV[i-1][ind-1].second >= l-1) {
+                AV[i].push_back({l, r});
+            } else if (ind < AV[i-1].size() && AV[i-1][ind].first < r) {
+                AV[i].push_back({AV[i-1][ind].first+1, r});
+            } 
+        }
+        ll r = 3*n;
+        ll l = forb[Z[i]].back() + 1;
+        ll ind = lower_bound(all(AV[i-1]), pl(l, 0)) - AV[i-1].begin();
+        
+        if (ind > 0 && AV[i-1][ind-1].second >= l-1) {
+            AV[i].push_back({l, r});
+        } else if (ind < AV[i-1].size() && AV[i-1][ind].first < r) {
+            AV[i].push_back({AV[i-1][ind].first+1, r});
+        } 
+        
+        cout << Z[i] << "|\n";
+        for (auto x : AV[i]) {
+            cout << x.first << " " << x.second << '\n';
+        }
+        cout << "-----------------" << endl;
+    }
+    
+    if (AV[m-1].empty()) MUN;
+    cout << AV[m-1][0].first << '\n';
 }
  
 signed main()
@@ -101,10 +180,10 @@ signed main()
     ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
     int size = 1;    
  
-	// freopen("cbarn.in", "r", stdin);
-    // freopen("cbarn.out", "w", stdout);
+	// freopen("kangaroo.in", "r", stdin);
+    // freopen("kangaroo.out", "w", stdout);
  
-    // cin >> size;
+    cin >> size;
     for (int i = 1; i <= size ; i++) solve(i);
     return 0;
 }
