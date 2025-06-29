3b0d2d924ecd9d7ebea70137426ac5b95261e904
zyadhany <zeyad.hany2003@gmail.com>
2025-06-29 15:18:38 +0300

    crt

--- solve-tmp/solve.cpp
+++ solve-tmp/solve.cpp
@@ -1,44 +1,47 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <bits/stdc++.h>
-#include <unordered_map>
-#include <unordered_set>
- 
+#include "bits/stdc++.h"
 #define ll long long
 #define ld long double
-#define pl pair<ll, ll>
 #define vi vector<ll>
-#define vii vector<vi>
-#define viii vector<vii>
-#define vc vector<char>
-#define vcc vector<vc>
-#define vp vector<pl>
-#define mi map<ll,ll>
-#define mc map<char,int>
-#define sortx(X) sort(X.begin(),X.end());
-#define all(X) X.begin(),X.end()
-#define allr(X) X.rbegin(),X.rend()
-#define ln '\n'
-#define YES {cout << "YES\n"; return;}
-#define NO {cout << "No\n"; return;}
-#define MUN {cout << "-1\n"; return;}
+#define all(X) X.begin(), X.end()
+
 using namespace std;
- 
-const int MODE = 998244353;
 
-void solve(int tc) {
+void solve() {
+    ll n;
 
+    cin >> n;
+
+    vi X(n+1);
+    vi P(n+1);
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> X[i];
+        P[i] = X[i] + P[i-1];
+    }
+    
+    stack<ll> st;
+    ll sol = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        while (!st.empty() && P[i-1] - P[st.top()-1] < X[i]) st.pop();
+        if (!st.empty()) sol += i - st.top();
+        st.push(i);
+    }
+    
+    cout << sol << '\n';
 }
 
-signed main()
-{
-    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-    int size = 1;    
+int main() {
+    int t = 1;
+    ios::sync_with_stdio(0), cin.tie(nullptr), cout.tie(nullptr);
 
-	// freopen("yinyang.in", "r", stdin);
-    // freopen("yinyang.out", "w", stdout);
- 
-    // cin >> size;
-    for (int i = 1; i <= size ; i++) solve(i);
-   
-    return 0;
+
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    
 }
