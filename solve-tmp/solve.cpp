6732eea80b30171003dfac1a2c9a42bc309d296a
zyadhany <zeyad.hany2003@gmail.com>
2025-07-01 13:38:26 +0300

    comb

--- solve-tmp/solve.cpp
+++ solve-tmp/solve.cpp
@@ -8,89 +8,166 @@
 #define vp vector<pl>
 using namespace std;
 
-const int SIZE = 1e6 + 1;
-const int MODE = 1e9+7;
-ll fac[SIZE], facinv[SIZE];
 
-ll nCr(ll n, ll r) {
-    if (n < r || n < 0) return 0;
-    ll res = fac[n];
-    res *= (facinv[r] * facinv[n - r]) % MODE;
-    return (res) % MODE;
-}
 
-ll catalan(int n)
+typedef vi item;
+
+class SegmentTree
 {
-    ll res = fac[2 * n];
-    res *= (facinv[n+1] * facinv[n]) % MODE;
-    return (res) % MODE;
-}
+public:
 
-void INIT() {
-    fill(fac, fac+SIZE, 1);
-    fill(facinv, facinv+SIZE, 1);
-    for (int i = 2; i < SIZE; i++) {
-        fac[i] = (i * fac[i - 1]) % MODE;
-        facinv[i] = MODE - MODE / i * facinv[MODE%i] % MODE;
+    void set(int index, ll value) {
+        set(0, 0, size - 1, index, value);
     }
-    for (int i = 2; i < SIZE; i++)
-        facinv[i] = (facinv[i] * facinv[i-1])%MODE;
-}
 
-ll gcdExtended(ll a, ll b, ll* x, ll* y)
-{
-    if (a == 0) {
-        *x = 0, *y = 1;
-        return b;
+    ll getrange(int l, int r) {
+        auto v = getrange(0, 0, size-1, l, r);
+        ll mx = 0;
+        for (auto a : v) mx = max(mx, mx^a);
+        return mx;
     }
-    ll x1, y1;
-    ll gcd = gcdExtended(b % a, a, &x1, &y1);
-    *x = y1 - (b / a) * x1;
-    *y = x1;
-    return gcd;
-}
 
-ll modeenv(ll n) {
-    ll x, y;
-    gcdExtended(n, MODE, &x, &y);
-    return (x + MODE) % MODE;
-}
+    void build(int n) {
+        size = 1;
+        while (size < n)
+            size *= 2;
+        tree.assign(size * 2, item());
+        lazy.assign(size * 2, 0);
+    }
 
-ll catalanKparts(ll n, ll k)
-{
-    ll res = ((k + 1) * nCr(2 * n + k, n)) % MODE;
-    res *= modeenv(n + 1 + k);
-    return (res) % MODE;
-}
+    void build(vi& X) {
+        size = 1;
+        while (size < X.size())
+            size *= 2;
+        tree.assign(size * 2, item());
+        lazy.assign(size * 2, 0);
 
-void solve() {
-    ll n;
-    string s;
-    cin >> n >> s;
+        build(X, 0, 0, size - 1);
+    }
 
-    ll at = 0;
-    for (auto a : s) {
-        if (a == '(') at++;
-        else at--;
-        if (at < 0) {
-            cout << "0\n";
+private:
+    int size;
+    vector<item> tree;
+    vector<long long> lazy;
+
+    item merge(item &a, item &b) {
+        item res = a;
+        for (auto x : b) {
+            for (int i = 0; i < (int)res.size(); i++) {  
+                x = min(x, x ^ res[i]);
+            }
+            if (x) res.push_back(x);
+        } 
+        return (res);
+    }
+
+    void set(int m, int lx, int rx, int pos, ll val) {
+        if (pos < lx || rx < pos) return;
+        if (lx == rx && lx == pos)
+        {
+            tree[m] = {val};
             return;
         }
+
+        int mid = (lx + rx) / 2;
+        item s1, s2;
+
+        set(m * 2 + 1, lx, mid, pos, val);
+        set(m * 2 + 2, mid + 1, rx, pos, val);
+        s1 = tree[m * 2 + 1], s2 = tree[m * 2 + 2];
+
+        tree[m] = merge(s1, s2);
     }
-    ll l = n - s.size() - at;
-    if (l < 0 || l%2) {
-        cout << "0\n";
-        return;      
+
+    item getrange(int m, int lx, int rx, int l, int r) {
+        if (rx < l || r < lx) return {};
+        if (l <= lx && rx <= r) return (tree[m]);
+
+        int mid = (lx + rx) / 2;
+        item s1, s2;
+
+        s1 = getrange(m * 2 + 1, lx, mid, l, r);
+        s2 = getrange(m * 2 + 2, mid + 1, rx, l, r);
+
+        return merge(s1, s2);
     }
-    ll sol = catalanKparts(l/2, at);  
-    cout << sol << '\n';
+
+    void build(vi& X, int m, int lx, int rx) {
+        if (lx == rx) {
+            if (lx < X.size()) tree[m] = {X[lx]};
+            return;
+        }
+
+        int mid = (lx + rx) / 2;
+        item s1, s2;
+
+        build(X, m * 2 + 1, lx, mid);
+        build(X, m * 2 + 2, mid + 1, rx);
+        s1 = tree[m * 2 + 1], s2 = tree[m * 2 + 2];
+
+        tree[m] = merge(s1, s2);
+    }
+};
+
+
+void solve() {
+    ll n, q;
+
+    cin >> n >> q;
+
+    vi X(n);
+
+    vector<set<ll>> bt(18);
+    cout << (1<<17) << '\n';
+    for (int i = 0; i < n; i++)
+    {
+        cin >> X[i];
+        for (int j = 0; j < 18; j++)
+            if ((X[i]>>j)&1) bt[j].insert(i);        
+    }
+    
+    SegmentTree sg;
+    sg.build(X);
+    
+    while (q--)
+    {
+        ll ty; cin >> ty;
+        if (ty == 1) {
+            ll l, r, x; cin >> l >> r >> x; l--, r--;
+            for (int i = 0; i < 18; i++)
+            {
+                if (!(x&(1<<i))) {
+                    auto it = bt[i].lower_bound(l);
+                    while (it != bt[i].end() && *it <= r)
+                    {
+                        X[*it] &= x;
+                        sg.set(*it, X[*it]);
+                        bt[i].erase(it);
+                        it = bt[i].lower_bound(l);
+                    }
+                }
+            }
+        } else if (ty == 2) {
+            ll ind, x; cin >> ind >> x;
+            for (int i = 0; i < 18; i++)
+                if (bt[i].count(ind)) bt[i].erase(ind);
+            X[ind] = x;
+            sg.set(ind, x);
+            for (int j = 0; j < 18; j++)
+                if ((x>>j)&1) bt[j].insert(ind);     
+        } else {
+            ll l, r; cin >> l >> r;
+            l--, r--;
+            cout << sg.getrange(l, r) << '\n';
+        }
+    }
+    
 }
 
 signed main() {
     ios::sync_with_stdio(0), cin.tie(nullptr), cout.tie(nullptr);
     ll t = 1;
-    INIT();
-    // cin >> t;
+    cin >> t;
     while (t--)
     {
         solve();
