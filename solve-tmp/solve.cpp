8478a5e7acfd3fc016123c78ef5167f6fc58185f
zyadhany <zeyad.hany2003@gmail.com>
2025-08-04 18:03:44 +0300

    math

--- solve-tmp/solve.cpp
+++ solve-tmp/solve.cpp
@@ -1,78 +1,109 @@
-// #pragma GCC optimize ("Ofast")
-// #pragma GCC target ("avx,avx2")
-// #pragma GCC optimize("unroll-loops")
-
-#define _CRT_SECURE_NO_WARNINGS
-#include <bits/stdc++.h>
-#include <unordered_map>
-#include <unordered_set>
-
-#define ll long long
-#define ld long double
-#define pl pair<ll, ll>
-#define vi vector<ll>
-#define vii vector<vi>
-#define vc vector<char>
-#define vcc vector<vc>
-#define vp vector<pl>
-#define mi map<ll,ll>
-#define mc map<char,int>
-#define sortx(X) sort(X.begin(),X.end());
-#define all(X) X.begin(),X.end()
-#define allr(X) X.rbegin(),X.rend()
-#define ln '\n'
-#define YES {cout << "YES\n"; return;}
-#define NO {cout << "NO\n"; return;}
-#define MUN {cout << "-1\n"; return;}
-
-const int MODE = 1e9+7;
-
+#include "bits/stdc++.h"
+#define ll  long long
 using namespace std;
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
+#define vi vector<int>
+#define vii vector<vi>
+#define pl pair<ll, ll>
+#define vp vector<pl>
+#define all(x) x.begin(),x.end()
 
-struct custom_hash {
-  static uint64_t splitmix64(uint64_t x) {
-    x += 0x9e3779b97f4a7c15;
-    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-    return x ^ (x >> 31);
-  }
-  size_t operator()(uint64_t x) const {
-    static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-    return splitmix64(x + FIXED_RANDOM);
-  }
-};
 
-void solve(int tc) {
-    ll n, x;
-
-    cin >> n >> x;
-
-gp_hash_table<int, int, custom_hash> mp;
-
-    for (int i = 0; i < n; i++)
-    {
-        ll a; cin >> a;
-        if (mp.find(x-a) != mp.end()) {
-            cout << mp[x-a] << ' ' <<i+1 << '\n';
-            return;
-        } 
-        mp[a] = i+1;
-    }
-    
-    cout << "IMPOSSIBLE\n";
+const int MAXSZ = 1e5+10;
+int prime[MAXSZ]={0};
+vi primes;
+void INIT(){
+    for(int i=0;i<MAXSZ;i++)prime[i]=i;
+    for(int i=2;i*i<MAXSZ;++i)if(prime[i]==i)
+    for(int j=i;j<MAXSZ;j+=i)prime[j]=i;
+    for(int i=2;i<MAXSZ;i++)if(prime[i]==i)primes.push_back(i);
+    prime[1] = 0;
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-    int size = 1;
+const int N = 9596;
+typedef bitset<N> item;
+ll primeind(ll x) {
+    ll re = lower_bound(all(primes), x) - primes.begin();
+    return re;
+}
 
-    //freopen("input.txt", "r", stdin);
-    //freopen("output.txt", "w", stdout);
-    // cin >> size;
-    for (int i = 1; i <= size; i++)
-        solve(i);
+const int MX = 1e5+10;
+bitset<N> bt[MX];
+
+void solve() {
+    ll n, q; cin >> n >> q;
+    vi X(n+1);
+    for (int i = 0; i < n; i++) cin >> X[i+1];
+    
+    vii adj(n+1);
+    for (int i = 0; i < n-1; i++)
+    {
+        ll u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+   
+    for (int i = 1; i <= n; i++)
+    {
+        if (prime[X[i]] == X[i]) {
+            bt[i][primeind(X[i])] = 1;
+        }
+    }
+
+    vi P(n+1);
+    function<void(ll, ll)> dfs = [&](ll u, ll p) -> void {
+        P[u] = p;
+        for (auto neg : adj[u]) if (neg != p) {
+            dfs(neg, u);
+            bt[u] |= bt[neg];
+        }
+    };
+    dfs(1, 0);
+    
+    while (q--)
+    {
+        ll ty; cin >> ty;
+        if (ty == 1) {
+            ll u; cin >> u;
+            cout << bt[u].count() << '\n';
+        } else if (ty == 2) {
+            ll u, x; cin >> u >> x;
+            ll ind = primeind(x);
+            cout << bt[u][ind] << '\n';
+        } else {
+            ll u, x; cin >> u >> x;
+            ll ind;
+            if (prime[X[u]]==X[u]) {
+                ind = primeind(X[u]);
+                ll at = u;
+                while (at)
+                {
+                    bt[at][ind] = 0;
+                    at = P[at];
+                }
+            }
+
+            X[u] = x;
+            if (prime[X[u]]==X[u]) {
+                ind = primeind(X[u]);
+                bt[u][ind] = 1;
+            }
+            ll at = u;
+            while (at)
+            {
+                for (auto neg : adj[at]) if (neg != P[at]) bt[at] |= bt[neg];
+                at = P[at];
+            }
+        }
+    }
+}
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(nullptr), cout.tie(nullptr);
+    int t = 1;
+    INIT();
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
 }
