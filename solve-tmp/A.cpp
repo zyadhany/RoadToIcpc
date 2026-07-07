c8c3d56ca2cc77882114f5b5caf604dad700e6fe
zyadhany <zeyad.hany2003@gmail.com>
2026-07-07 13:22:25 +0300

    fs

--- solve-tmp/A.cpp
+++ solve-tmp/A.cpp
@@ -22,71 +22,52 @@
 #define allr(X) X.rbegin(),X.rend()
 #define ln '\n'
 #define YES {cout << "YES\n"; return;}
-#define NO {cout << "NO\n"; return;}
+#define NO {cout << "No\n"; return;}
 #define MUN {cout << "-1\n"; return;}
 
-const int MODE = 1e9+7;
-const int MOD2 = (MODE + 1) / 2;
 using namespace std;
 
+const int MODE = 998244353;
+const ll INF = 1e16;
 
-void solve(int tc) {
-    ll n;
-
-    cin >> n;
-
-    vii adj(n+1);
-    for (int i = 0; i < n-1; i++)
+bool isplan(ll n) {
+    string s = to_string(n);
+    for (int i = 0; i < s.size(); i++)
     {
-        ll u, v; cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        if (s[i] != s[s.size()-i-1]) return false;
     }
-    
-    vi dp(n+1), C(n+1), dp2(n+1);
-    ll sol = 0;
-
-    function<void(ll, ll)> dfs = [&](ll u, ll p) {
-        ll mx = 0;
-        for (auto neg : adj[u]) if(neg != p) {
-            dfs(neg, u);
-            C[u] += C[neg];
-            mx = max(mx, dp[neg]);
-        }
-        dp[u] = mx+C[u];
-    };
-    dfs(1, 0);
-
-    function<void(ll, ll, ll)> dfs2 = [&](ll u, ll p, ll tp) {
-        pl mx = {tp, 0};
-        
-        for (auto neg : adj[u]) if(neg != p) {
-            if (dp[neg] > mx.first) {
-                swap(mx.first, mx.second);
-                mx.first = dp[neg];
-            } else if (dp[neg] > mx.second) {
-                mx.second = dp[neg];
-            }
-        }
-        sol = max(sol, n+mx.first);
-
-        for (auto neg : adj[u]) if(neg != p) {
-            ll re;
-            if (dp[neg] == mx.first) {
-                re = n - C[neg] + mx.second;
-            } else re = mx.first + n - C[neg];
-            
-            dfs2(neg, u, re);
-        }
-    };
-    dfs2(1, 0, 0);
-
-
-    cout << sol << '\n';
+    return true;
 }
 
+const int N = 2e6+10;
+vi IDN[N];
 
-int main()
+void solve(int tc) {
+    vi P;
+    ll cnt = 0;
+    for (int i = 2; i < N; i++)
+    {
+        if (isplan(i)) P.push_back(i);
+    }
+
+    ll n, q;
+
+    cin >> n >> q;
+    vi X(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> X[i];
+    }
+
+    vii Q;
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+    }
+        
+}
+
+signed main()
 {
     ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
     int size = 1;
@@ -94,6 +75,7 @@ int main()
     //freopen("input.txt", "r", stdin);
     //freopen("output.txt", "w", stdout);
 
-    cin >> size;
-    for (int i = 1; i <= size; i++) solve(i);
+    // cin >> size;
+    for (int i = 1; i <= size; i++)
+        solve(i);
 }
--- solve-tmp/solve.cpp
+++ solve-tmp/solve.cpp
@@ -22,77 +22,263 @@
 #define allr(X) X.rbegin(),X.rend()
 #define ln '\n'
 #define YES {cout << "YES\n"; return;}
-#define NO {cout << "NO\n"; return;}
+#define NO {cout << "No\n"; return;}
 #define MUN {cout << "-1\n"; return;}
 
-const int SIZE = 1e6 + 1;
-const int MODE = 998244353;
-const ll INF = 2e16;
 using namespace std;
 
+const int MODE = 1e9+7;
+const ll INF = 1e16;
 
+
+
+/**
+ * usage:-
+ * creat tree element.
+ * SegmentTree sg;
+ * 
+ * Functions you can use:
+ * @set: set index or range to value.
+ * @geteange: get value of given range.
+ * @build: build tree with given vector or size.
+ * 
+ * make sure to look at item typedef.
+ * you can change merge function to change it's oppration.
+ * it you want to make change to segment work in checkLazy().
+*/
+
+typedef long long item;
+/*
+struct item
+{
+    int val;
+
+    item(){
+        val = 0;
+    }
+};
+*/
+
+class SegmentTree
+{
+public:
+
+    void set(int index, ll value) {
+        set(0, 0, size - 1, index, value);
+    }
+    void set(int l, int r, ll value) {
+        set(0, 0, size - 1, l, r, value);
+    }
+
+    item getrange(int l, int r) {
+        return (getrange(0, 0, size - 1, l, r));
+    }
+
+    void build(int n) {
+        size = 1;
+        while (size < n)
+            size *= 2;
+        tree.assign(size * 2, item());
+        lazy.assign(size * 2, 0);
+    }
+
+    void build(vi& X) {
+        size = 1;
+        while (size < X.size())
+            size *= 2;
+        tree.assign(size * 2, item());
+        lazy.assign(size * 2, 0);
+
+        build(X, 0, 0, size - 1);
+    }
+
+private:
+    int size;
+    vector<item> tree;
+    vector<long long> lazy;
+
+    item merge(item &a, item &b) {
+        item res = (a*b)%MODE;
+        return (res);
+    }
+
+    void checkLazy(int m, int lx, int rx) {
+        if (!lazy[m]) return;
+        tree[m] = lazy[m];
+        
+        if (lx != rx) {
+            lazy[2 * m + 1] += lazy[m];
+            lazy[2 * m + 2] += lazy[m];
+        }
+
+        lazy[m] = 0;
+    }
+
+    void set(int m, int lx, int rx, int pos, ll val) {
+        checkLazy(m, lx, rx);
+        if (pos < lx || rx < pos) return;
+        if (lx == rx && lx == pos)
+        {
+            tree[m] = (tree[m] * val) % MODE;
+            return;
+        }
+
+        int mid = (lx + rx) / 2;
+        item s1, s2;
+
+        set(m * 2 + 1, lx, mid, pos, val);
+        set(m * 2 + 2, mid + 1, rx, pos, val);
+        s1 = tree[m * 2 + 1], s2 = tree[m * 2 + 2];
+
+        tree[m] = merge(s1, s2);
+    }
+
+    void set(int m, int lx, int rx, int l, int r, ll val) {
+        checkLazy(m, lx, rx);
+        if (rx < l || r < lx) return;
+        if (l <= lx && rx <= r)
+        {
+            lazy[m] = val;
+            checkLazy(m, lx, rx);
+            return;
+        }
+
+        int mid = (lx + rx) / 2;
+        item s1, s2;
+
+        set(m * 2 + 1, lx, mid, l, r, val);
+        set(m * 2 + 2, mid + 1, rx, l, r, val);
+        s1 = tree[m * 2 + 1], s2 = tree[m * 2 + 2];
+
+        tree[m] = merge(s1, s2);
+    }
+
+    item getrange(int m, int lx, int rx, int l, int r) {
+        checkLazy(m, lx, rx);
+        if (rx < l || r < lx) return (1);
+        if (l <= lx && rx <= r) return (tree[m]);
+
+        int mid = (lx + rx) / 2;
+        item s1, s2;
+
+        s1 = getrange(m * 2 + 1, lx, mid, l, r);
+        s2 = getrange(m * 2 + 2, mid + 1, rx, l, r);
+
+        return merge(s1, s2);
+    }
+
+    void build(vi& X, int m, int lx, int rx) {
+        if (lx == rx) {
+            if (lx < X.size()) tree[m] = item(X[lx]);
+            return;
+        }
+
+        int mid = (lx + rx) / 2;
+        item s1, s2;
+
+        build(X, m * 2 + 1, lx, mid);
+        build(X, m * 2 + 2, mid + 1, rx);
+        s1 = tree[m * 2 + 1], s2 = tree[m * 2 + 2];
+
+        tree[m] = merge(s1, s2);
+    }
+};
+
+SegmentTree sg;
+const int N = 1e5+10;
 
 void solve(int tc) {
-    ll n, m;
+    ll n, q;
 
-    cin >> n >> m;
+    cin >> n >> q;
 
-    vi X(n), P(1);
+    vi X(n+1), Y(n+1);
+
     for (int i = 0; i < n; i++)
     {
-        cin >> X[i];
-        ll v = X[i];
-        while (v--) P.push_back(i);
-        if (i) X[i] += X[i-1];
+        cin >> X[i+1];
     }
-    for (int i = 1; i <= m; i++) P[i] += P[i-1];
-    
-    
-    vii dp(m+1, vi(m+1, INF));
-    for (int i = 0; i <= m; i++)
+    for (int i = 0; i < n; i++)
     {
-        if (i < X[0]) dp[i][i] = X[0]-i;
-        else dp[i][i] = P[i];
+        cin >> Y[i+1];
+    }
+    
+    vii adj(n+1);
+    for (int i = 0; i < n-1; i++)
+    {
+        ll u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
 
-    for (ll j = m-1; j >= 0; j--)
-        for (ll k = m; k >= 0; k--)
-            dp[j][k] = min(dp[j][k], dp[j+1][k]);   
-    // cout << dp[4][4] << ' ' << dp[3][4] << "|\n";
-    for (int i = 1; i < n; i++)
+    vector<vii> Q(n+1);
+    for (int i = 0; i < q; i++)
     {
-        vii TM(m+1, vi(m+1, INF));
-        for (ll j = 0; j <= m; j++)
-        {
-            for (ll k = j; k <= m; k++)
-            {
-                ll ad = 0;
-                if (X[i] >= k) ad = X[i]-k;
-                else {
-                    ll tk = min(j, k-X[i]);
-                    // if (tk > j) break;
-                    ad = P[k] - P[k-tk] - i * tk;
-                }
-                TM[j][k] = ad + dp[j][k-j];
-                // cout << j << ' ' << k << ' ' << TM[j][k] << "|\n";
-            }            
+        ll u, l, r; cin >> u >> l >> r;
+        Q[u].push_back({i, l, r});
+    }
+
+    vi res(q);
+    vi S(n+1);
+    function<ll(ll, ll)> dfs2 = [&](ll u, ll p) -> ll {
+        ll sz = 1;
+        for (auto neg : adj[u]) if (neg != p) {
+            sz += dfs2(neg, u);
         }
-        // cout << "||||||||||||\n";
-        for (ll j = m-1; j >= 0; j--)
-            for (ll k = m-1; k >= 0; k--)
-                TM[j][k] = min(TM[j][k], TM[j+1][k]);        
-        swap(dp, TM);
-        cout << dp[1][9] << "|\n";
-    }
-    
+        return sz;
+    };
 
-    ll sol = INF;
-    for (int i = 0; i <= m; i++) sol = min(sol, dp[i][m]);
-    cout << sol << '\n';
+    sg.build(N);
+    function<vp(ll, ll)> dfs = [&](ll u, ll p) -> vp {
+        vp Z(1, {X[u], Y[u]});
+        sg.set(1, N-1, 1);
+        cout << u << ' ' << sg.getrange(2, 2) << "||\n";
+        sg.set(Y[u], X[u]);
+        sort(all(adj[u]), [&](ll a, ll b) {
+            return S[a] < S[b];
+        });
+
+        vector<vp> K(adj[u].size());
+        ll at = 0;
+        for (auto neg : adj[u]) if (neg != p) {
+            sg.set(1, N-1, 1);
+            K[at] = dfs(neg, u);
+            swap(Z, K[at]);
+            at++;
+        }
+        for (auto &H : K) {
+            if (Z.size() < H.size()) swap(H, Z);
+            for (auto a : H) Z.push_back(a);
+
+            for (auto [a, b] : H) {
+                // sg[b] -> a
+                sg.set(b, a);
+            }
+        }
+        // if (u == 3) 
+        
+        for (auto &H : Q[u]) {
+            ll ind = H[0], l = H[1], r = H[2];
+            // res[ind] = sg[l,r];
+            res[ind] = sg.getrange(l, r);
+        }
+        return Z;
+    };
+    dfs2(1, 0);
+    dfs(1, 0);
+
+    for (auto a: res) cout << a << '\n';
 }
 
-int main() {
+signed main()
+{
     ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
     int size = 1;
-    for (int i = 1; i <= size; i++) solve(i);
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    // cin >> size;
+    for (int i = 1; i <= size; i++)
+        solve(i);
 }
